f7ff2187631d1d356bc27ed05ae4a20573dda0f6
anmol dhanda <rk352714@gmail.com>
2024-03-03 15:57:06 +0530

    printed various countries time using tolocalestring in dateobject and passing each country's unique timezone in tolocalestring

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const indiatime = document.getElementById("indiatime");
+const europe_berlintime = document.getElementById("europe-berlintime");
 const usatime = document.getElementById("usatime");
 const romaniatime = document.getElementById("romaniatime");
 
 const updatetime = () => {
-    let currentdatetime = new Date();
-    usatime.innerHTML = currentdatetime.toLocaleString("en-US", {
-        timeZone: "America/Los_Angeles",
-    })
-    console.log(usatime.textContent);
-}
+  let currentdatetime = new Date();
+  usatime.innerHTML = currentdatetime.toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+  });
+  // console.log(usatime.textContent);
+  europe_berlintime.innerHTML = currentdatetime.toLocaleString("de-DE", {
+    timeZone: "Europe/Berlin",
+  });
+  // console.log(europe_berlintime.textContent);
+  romaniatime.innerHTML = currentdatetime.toLocaleString("ro-RO", {
+    timeZone: "Europe/Bucharest",
+  });
+  // console.log(romaniatime.textContent);
+};
 
 // ====================== update time after each and every single second ======================
 setInterval(updatetime, 1000);
